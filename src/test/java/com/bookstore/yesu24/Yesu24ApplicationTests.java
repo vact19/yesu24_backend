85d591ecaa4607a32761bd0667de61be35ceee50
vact19 <vact19@naver.com>
2022-08-31 09:26:32 +0900

    initial commit

---
+++ src/test/java/com/bookstore/yesu24/Yesu24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.yesu24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yesu24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
