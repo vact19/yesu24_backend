1629993ac2563fa2677644fa20716c8271525ae7
vact19 <chj6703@gmail.com>
2022-12-27 23:48:11 +0900

    테스트 성공

--- src/test/java/com/bookstore/yesu24/Yesu24ApplicationTests.java
+++ src/test/java/com/bookstore/yesu24/Yesu24ApplicationTests.java
@@ -1,13 +1,16 @@
 package com.bookstore.yesu24;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+
 class Yesu24ApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
