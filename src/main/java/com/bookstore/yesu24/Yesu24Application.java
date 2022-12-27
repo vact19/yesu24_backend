0b167eb6bd54e3c5a73ed7551e6232a746b3e5a8
vact19 <chj6703@gmail.com>
2022-12-27 22:57:44 +0900

    실행준비 - DB설정미완

--- src/main/java/com/bookstore/yesu24/Yesu24Application.java
+++ src/main/java/com/bookstore/yesu24/Yesu24Application.java
@@ -8,6 +8,14 @@ public class Yesu24Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Yesu24Application.class, args);
+		System.out.println("====시스템 속성====");
+		System.getProperties().list(System.out);
+		System.out.println("====환경 변수====");
+		System.out.println(System.getenv());
+		System.out.println("====프로그램 인자====");
+		for (String arg : args) {
+			System.out.print(arg + " | ");
+		}
 	}
 
 }
--- src/main/java/com/bookstore/yesu24/domain/genre/bookgenre/entity/BookGenre.java
+++ src/main/java/com/bookstore/yesu24/domain/genre/bookgenre/entity/BookGenre.java
@@ -15,4 +15,8 @@ public class BookGenre {
 
     @Column(length = 30, nullable = false)
     private String genreName;
+
+    // 1번장르 더미데이터 만들어두고, new Book(new BookGenre(1)) 이렇게 PK만으로 객체 생성하고 save(book)했을 때,
+    // 알아서 book의 BookGenre에 더미데이터의 genreName값이 들어가는지 확인.
+    // ID를 명시하고 save했으니까 당연히 merge가 일어나서 값이 합쳐지나?(둘째줄 주석처럼 되나?)
 }
